574e97f8dcc9e5ae83fbc17f5eba36b713fe76db
Diana Kalieva <dicharmed@MacBook-Pro-Diana.local>
2024-04-05 20:59:51 +0500

    transport-filter: ui is done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { TransportFilter } from "./features/transport-filter/transport-filter";
+
+function App() {
+  return (
+    <>
+      <TransportFilter />
+    </>
+  );
+}
+
+export default App;
