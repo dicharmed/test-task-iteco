77005cbd55613d1a4a9c3a793811a71f7567aba9
Diana Kalieva <dicharmed@MacBook-Pro-Diana.local>
2024-04-05 22:50:24 +0500

    transport-filter: Formik is added

---
+++ src/features/transport-filter/components/transport-filter-component.tsx
@@ -0,0 +1,118 @@
+import React from "react";
+import { Button, Card, DatePicker, Flex, Input, Typography } from "antd";
+import SwapIcon from "../../../assets/swap-icon.svg?react";
+import styled from "styled-components";
+const { Title, Text } = Typography;
+
+const CardContent = styled.form`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: repeat(3, 48px);
+  justify-content: space-between;
+  align-items: center;
+  grid-gap: 15px 10px;
+`;
+const TitleStyled = styled(Title)`
+  text-align: left;
+`;
+
+const InputStyled = styled(Input)<{
+  $borderColor?: string;
+  $paddingLeft?: string;
+}>`
+  height: 48px;
+  padding-left: ${(props) => props.$paddingLeft || "auto"};
+  border-color: ${(props) => props.$borderColor || "#d9d9d9"};
+`;
+
+const DatePickerStyled = styled(DatePicker)`
+  height: 100%;
+`;
+
+const SearchBtn = styled(Button)`
+  height: 100%;
+  background-color: #ff9a19;
+  border-color: #ff9a19;
+  text-transform: uppercase;
+  font-size: 16px;
+  color: white;
+`;
+const InputGroup = styled(Flex)`
+  position: relative;
+  align-items: center;
+`;
+const SwapIconBtn = styled(Button)`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-57%, -50%);
+  z-index: 1;
+`;
+const ButtonLink = styled(Button)`
+  padding: 0;
+`;
+
+type Props = {
+  handleSubmit: (e: React.FormEvent<HTMLFormElement> | undefined) => void;
+  handleChange: (e: React.ChangeEvent) => void;
+  handleDatePickerChange: (date: unknown) => void;
+  values: {
+    from: string;
+    to: string;
+    orderNumber: string;
+  };
+};
+export const TransportFilterComponent: React.FC<Props> = ({
+  handleSubmit,
+  handleChange,
+  handleDatePickerChange,
+  values,
+}): React.ReactElement => {
+  const { orderNumber, from, to } = values;
+  return (
+    <Card>
+      <CardContent onSubmit={handleSubmit}>
+        <TitleStyled level={4}>Поиск грузов</TitleStyled>
+        <InputStyled
+          name="orderNumber"
+          placeholder="№ заказа"
+          value={orderNumber}
+          onChange={handleChange}
+        />
+
+        <InputGroup gap="small">
+          <InputStyled
+            name="from"
+            $borderColor="green"
+            placeholder="Откуда"
+            value={from}
+            onChange={handleChange}
+          />
+          <SwapIconBtn shape="circle" icon={<SwapIcon />} />
+          <InputStyled
+            name="to"
+            $borderColor="green"
+            placeholder="Куда"
+            $paddingLeft="20px"
+            value={to}
+            onChange={handleChange}
+          />
+        </InputGroup>
+        <DatePickerStyled
+          name="date"
+          placeholder="Дата погрузки"
+          onChange={(date) => handleDatePickerChange(date)}
+        />
+
+        <Flex justify="flex-end">
+          <ButtonLink type="link" htmlType="button">
+            <Text type="secondary" underline>
+              Сбросить фильтры
+            </Text>
+          </ButtonLink>
+        </Flex>
+        <SearchBtn htmlType="submit">Поиск</SearchBtn>
+      </CardContent>
+    </Card>
+  );
+};
--- src/features/transport-filter/transport-filter.tsx
+++ src/features/transport-filter/transport-filter.tsx
@@ -1,83 +1,25 @@
 import React from "react";
-import { Button, Card, DatePicker, Flex, Input, Typography } from "antd";
-import SwapIcon from "../../assets/swap-icon.svg?react";
-import styled from "styled-components";
-const { Title, Text } = Typography;
+import { useFormik } from "formik";
+import { TransportFilterComponent } from "./components/transport-filter-component";
 
-const CardContent = styled.div`
-  display: grid;
-  grid-template-columns: 2fr 1fr;
-  grid-template-rows: repeat(3, 48px);
-  justify-content: space-between;
-  align-items: center;
-  grid-gap: 15px 10px;
-`;
-const TitleStyled = styled(Title)`
-  text-align: left;
-`;
-
-const InputStyled = styled(Input)<{
-  $borderColor?: string;
-  $paddingLeft?: string;
-}>`
-  height: 48px;
-  padding-left: ${(props) => props.$paddingLeft || "auto"};
-  border-color: ${(props) => props.$borderColor || "#d9d9d9"};
-`;
-
-const DatePickerStyled = styled(DatePicker)`
-  height: 100%;
-`;
-
-const SearchBtn = styled(Button)`
-  height: 100%;
-  background-color: #ff9a19;
-  border-color: #ff9a19;
-  text-transform: uppercase;
-  font-size: 16px;
-  color: white;
-`;
-const InputGroup = styled(Flex)`
-  position: relative;
-  align-items: center;
-`;
-const SwapIconBtn = styled(Button)`
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-57%, -50%);
-  z-index: 1;
-`;
-const ButtonLink = styled(Button)`
-  padding: 0;
-`;
 export const TransportFilter: React.FC = (): React.ReactElement => {
+  const formik = useFormik({
+    initialValues: {
+      from: "",
+      to: "",
+      orderNumber: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
-    <Card>
-      <CardContent>
-        <TitleStyled level={4}>Поиск грузов</TitleStyled>
-        <InputStyled placeholder="№ заказа" />
-
-        <InputGroup gap="small">
-          <InputStyled $borderColor="green" placeholder="Откуда" />
-          <SwapIconBtn shape="circle" icon={<SwapIcon />} />
-          <InputStyled
-            $borderColor="green"
-            placeholder="Куда"
-            $paddingLeft="20px"
-          />
-        </InputGroup>
-        <DatePickerStyled placeholder="Дата погрузки" />
-
-        <Flex justify="flex-end">
-          <ButtonLink type="link">
-            <Text type="secondary" underline>
-              Сбросить фильтры
-            </Text>
-          </ButtonLink>
-        </Flex>
-        <SearchBtn>Поиск</SearchBtn>
-      </CardContent>
-    </Card>
+    <TransportFilterComponent
+      values={formik.values}
+      handleSubmit={formik.handleSubmit}
+      handleChange={formik.handleChange}
+      handleDatePickerChange={(date) => formik.setFieldValue("date", date)}
+    />
   );
 };
